f1e82b6d568584dcd81b69c9551ea8b936dae561
hanxuetao <my@email.com>
2017-11-30 13:39:01 +0200

    ui-layout

--- src/App.js
+++ src/App.js
@@ -1,113 +1,59 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
-import Input, { InputLabel } from 'material-ui/Input';
-import { FormControl, FormHelperText } from 'material-ui/Form';
-import Select from 'material-ui/Select';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import 'jquery/dist/jquery.min.js';
+import './App.css';
 
 
-//Here you can set your css like that format below 
-const styles = theme => ({
-    root: {
-        flexGrow: 1,
-        marginTop: 30,
-    },
-    paper: {
-        padding: 16,
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
-    },
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-    },
-    formControl: {
-        margin: theme.spacing.unit,
-        minWidth: 200,
-    },
-    selectEmpty: {
-        marginTop: theme.spacing.unit * 2,
-    },
-});
+
 
 class App extends Component{
 
-    state = {
-        age: '',
-        name: 'hai',
-    };
-    handleChange = name => event => {
-        this.setState({ [name]: event.target.value });
-    };
-
-
     render() {
 
-        const {classes} = this.props;
-
-        // Here is my project layout
-        // <Grid item md={3} sm={3} name="left"> is for the left part
-        // <Grid item md={6} sm={3} name="chart"> for chart
-        // <Grid item md={3} sm={3} name="right"> for right
-        // When you decide to add another elements you need you set className={class.YourCss}
-
         return (
-            <div className={classes.container}>
-                <Grid container spacing={0}>
-                    <Grid item xs={12}>
-                        <Paper className={classes.paper}>Metsämittari</Paper>
-                    </Grid>
-                    <Grid item md={3} sm={3} name="left">
-                        <h2 className={classes.paper}>Skenaarioden valinta</h2>
-                        <FormControl className={classes.formControl}>
-                            <InputLabel htmlFor="age-native-simple">Age</InputLabel>
-                            <Select
-                                native
-                                value={this.state.age}
-                                onChange={this.handleChange('age')}
-                                input={<Input id="age-native-simple" />}
-                            >
-                                <option value="" />
-                                <option value={10}>Ten</option>
-                                <option value={20}>Twenty</option>
-                                <option value={30}>Thirty</option>
-                            </Select>
-                        </FormControl>
-                        <FormControl className={classes.formControl}>
-                            <InputLabel htmlFor="age-native-helper">Age</InputLabel>
-                            <Select
-                                native
-                                value={this.state.age}
-                                onChange={this.handleChange('age')}
-                                input={<Input id="age-native-helper" />}
-                            >
-                                <option value="" />
-                                <option value={10}>Ten</option>
-                                <option value={20}>Twenty</option>
-                                <option value={30}>Thirty</option>
-                            </Select>
-                            <FormHelperText>Some important helper text</FormHelperText>
-                        </FormControl>
-                    </Grid>
-                    <Grid item md={6} sm={3} name="chart">
-                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
-                    </Grid>
-                    <Grid item md={3} sm={3} name="right">
-                        <Paper className={classes.paper}>xs=6 sm=3</Paper>
-                    </Grid>
-                </Grid>
+         <div className="container">
+             <h1 className="title">Metsämittari </h1>
+            <div className="row">
+
+
+                <div className="col-4 col-md-2">
+                    <div className="dropdown">
+                        <h3 className="title">Skenaarioiden valinta</h3>
+                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown button
+                        </button>
+                        <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <a className="dropdown-item">Action</a>
+                            <a className="dropdown-item">Another action</a>
+                            <a className="dropdown-item">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="col-12 col-md-8">.col-6 .col-md-4</div>
+
+
+                <div className="col-4 col-md-2">
+                    <h3 className="title">Indikaattoreiden valinta</h3>
+                    <div className="list-group">
+                        <button type="button" className="list-group-item list-group-item-action active">
+                            Cras justo odio
+                        </button>
+                        <button type="button" className="list-group-item list-group-item-action">Dapibus ac facilisis in</button>
+                        <button type="button" className="list-group-item list-group-item-action">Morbi leo risus</button>
+                        <button type="button" className="list-group-item list-group-item-action">Porta ac consectetur ac</button>
+                        <button type="button" className="list-group-item list-group-item-action" disabled>Vestibulum at eros</button>
+                    </div>
+                </div>
             </div>
-
-
-
+         </div>
         );
     }
 }
-App.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
 
 
-export default withStyles(styles)(App);
+
+export default App;
