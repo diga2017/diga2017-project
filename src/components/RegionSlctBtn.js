7d88f1247c59aa8150c82ce2004203bc167ebd06
JackiJang <t5istu01@students.oamk.fi>
2017-12-05 12:57:15 +0200

    r change

--- src/components/RegionSlctBtn.js
+++ src/components/RegionSlctBtn.js
@@ -1,52 +1,36 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Data from "../data/Data";
 import React, { PropTypes } from "react";
-import SelectItem from "react-select-item";
+//import SelectItem from "react-select-item";
 //import { element } from "./C:/Users/Omistaja/AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/prop-types";
 
 class RegionLvlBtn extends React.Component {
-  static defaultProps = {
-    placeholder: "Valitse aluetaso:"
-  };
+  constructor(props){
+    super(props);        
+    this.selectHandler = this.selectHandler.bind(this);      
+}
 
-  /**
-   * Component constructor
-   * The component is depended from Bootstrap 3.x (styles only)
-   * @param props
-   */
-  constructor(props) {
-    super(props);
-    this.handleMultiChange = this.handleMultiChange.bind(this);
-  }
-
-  handleMultiChange(event) {
-    this.props.regionLevels(event.target.value);
-  }
-
-  render() {
-    const {regionLevels} =  this.state;  
+selectHandler(event){     
+    this.Data.getRegionLevels()(event.target.value); 
+}
+  
+render () {  
+    const {regionLevels} =  this.props;      
     return (
-      <div className={this.props.wrapperClassName}>
-        <div className="form-group">
-          <label>{this.props.label}</label>
-          <SelectItem
-            label={this.props.label}
-            onChange={this.handleMultiChange}
-            value={this.state.regionLevels}
-            closeText={false}
-            className="form-control"
-            multiple={true}
-          >
-            {this.state.regionLevels.map(element => (
-              <option value={element.name} key={element.id}>
-                {element.name}
-              </option>
-            ))}
-          </SelectItem>
+        <div className="RegionLevel">  
+            <h2 className="headIndicator">Aluetaso:</h2> 
+            <label className="textfont">Aluetaso: </label>            
+           <div className="form-group">                    
+                <select className="form-control"   onChange={this.selectHandler} >
+                    {
+                        regionLevels.map(element=>
+                        <option value={element.id} key={element.id} >{element.name}</option>) 
+                    }                       
+                </select>
+            </div>
         </div>
-      </div>
-    );
-  }
+    )
+}
 }
 
 export default RegionLvlBtn;
