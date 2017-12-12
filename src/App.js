ca6b8d616d056b8a25967276a33bdae794357b77
Mark_Waldorn <C15501723@mydit.ie>
2017-12-12 10:35:56 +0200

    NavBar commponent

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import ScenarioColBtn from "./components/DropDownScnCol";
 import Scenarios from "./components/DropDownScns";
 import RegionLvlBtn from "./components/DropDownRgnSlct";
 import RegionBtn from "./components/DropDownRegions";
+import NavBar from "./components/NavBar";
 
 import Graphs from './components/Graphs'
 class App extends Component {
@@ -77,24 +78,8 @@ class App extends Component {
     return (
       
       <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ padding: 2 }}>
-          <div className="container">
-              <a className="navbar-brand text-white" href="#" >diga2017</a>
-              
-              <div className="form-inline my-2 my-lg-0">
-
-              <button className="Toggle_langage" onClick = {this.toggleLanguage}>{ localizedStrings.languageOnSwitch } 
-              
-              <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1280px-Flag_of_the_United_Kingdom.svg.png" width="30" height="30" alt="" />
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/2000px-Flag_of_Finland.svg.png" width="30" height="30" alt="" /></button>
-              
-              </div>
-
-              
-              
-          </div>
-      </nav>
-
+     
+      <NavBar />
 
 
        <div className="container">
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,20 +8,20 @@ class NavBar extends Component {
 
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{ padding: 2 }}>
-                    <div className="container">
-                        <a className="navbar-brand text-white" href="#" >diga2017</a>
-                        
-                        <div className="form-inline my-2 my-lg-0">
-
-                        <button type="button" className="btn btn-primary" onClick={this.toggleLanguage}>{ localizedStrings.languageOnSwitch }</button>
-
-                        <button className="english_box" onClick = {this.toggleLanguage}>{ localizedStrings.languageOnSwitch } <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1280px-Flag_of_the_United_Kingdom.svg.png" width="30" height="30" alt="" /></button>
-                        <button className="finish_box" onClick = ""> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/2000px-Flag_of_Finland.svg.png" width="30" height="30" alt="" /></button>
-                        </div>
-                        
-                    </div>
-                </nav>
-            </div>
+            <div className="container">
+                <a className="navbar-brand text-white" href="#" >diga2017</a>
+                
+                <div className="form-inline my-2 my-lg-0">
+  
+                <button className="Toggle_langage" onClick = {this.toggleLanguage}>{ localizedStrings.languageOnSwitch } 
+                
+                <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1280px-Flag_of_the_United_Kingdom.svg.png" width="30" height="30" alt="" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Finland.svg/2000px-Flag_of_Finland.svg.png" width="30" height="30" alt="" /></button>
+                
+                </div>
+                </div>
+            </nav>
+        </div>
    
         
         )
