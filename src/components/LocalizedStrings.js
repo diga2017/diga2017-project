4595343015680ff3d3a11a5e742b8fa3144de9f7
Teemu Karjalainen <t5kate01@students.oamk.fi>
2017-12-14 13:06:46 +0200

    Removed useless things, dropdowns update
    Dropwdowns now update better when language is changed.
    removed useless/unused things.

--- src/components/LocalizedStrings.js
+++ src/components/LocalizedStrings.js
@@ -25,7 +25,7 @@ let localizedStrings = new LocalizedStrings({
     titleScenarioCollection: "Skenaariokokoelma",
     titleScenarios: "Skenaariot",
     titleTimePeriod: "Ajankohta",
-    titleChoosingIndicators: "Indikaattoreriden valinta",
+    titleChoosingIndicators: "Indikaattoreiden valinta",
     sendFeedback: "Palaute",
     stayOpen: "Pysy avoinna kun skenaario valittu"
   }
